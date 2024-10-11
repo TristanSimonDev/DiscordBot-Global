755d91eb400d422c8d6ea8eb40840eba5786bbf2
Tristan | S. <simontristandev@gmail.com>
2024-10-11 16:38:56 +0200

    run the functions dynamically

--- src/Events/Client-Ready.js
+++ src/Events/Client-Ready.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js')
+const fs = require('fs')
 
 module.exports = {
-    Name: Discord.Events.ClientReady,
-    Type: 'on',
-    EventId: 1,
+    name: Discord.Events.ClientReady,
+    type: 'on',
+    id: 1,
 
     async execute(client) {
         if (!(client instanceof Discord.Client)) return;
 
-        console.log(client.user.tag)
+        const EventFiles = fs.readdirSync("src/Event-Functions/Client-Ready").filter(file => file.endsWith('.js'))
+
+
+        for (const file of EventFiles) {
+            const Event = require(`../Event-Functions/Client-Ready/${file}`)
+
+            Event.execute(client)
+        }
+
+
+
+        
 
         
     }
