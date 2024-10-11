e08933f059ad2a645a6f6f89a1fb59340f1a2335
Tristan | S. <simontristandev@gmail.com>
2024-10-11 14:30:49 +0200

    env performance

--- src/index.js
+++ src/index.js
@@ -14,9 +14,53 @@ const EventFiles = fs.readdirSync("src/Events").filter(file => file.endsWith(".j
 for (const file of EventFiles) {
 
     const Event = require(`./Events/${file}`)
-    console.log(ColorOutput(`Event: ${Event.Name} with EventId ${Event.Id} loaded successfully`).green)
+
+    console.log(ColorOutput(`Event: [${Event.Name}] with EventId [${Event.Id}] loaded successfully`).green)
     client.on(Event.Name, async (...args) => Event.execute(...args))
 
 }
 
 client.login(process.env.TOKEN)
+.then(() => {
+    console.log(
+        ColorOutput(
+            `\n=== Bot Login Info ===` +
+                `\n\nToken Length:                 ${process.env.TOKEN.length}` +
+                `\nNode Version:                   ${process.version}` +
+                `\nDevice Architecture:            ${process.arch}` +
+                `\nDiscord.js Version:             ${
+                    require("discord.js").version
+                }` +
+                `\nOS:                             ${process.platform}` +
+                `\nUptime:                         ${Math.floor(
+                    process.uptime()
+                )} seconds` +
+                `\nMemory Used:                    ${Math.round(
+                    (process.memoryUsage().heapUsed / 1024 / 1024) * 100
+                ) / 100} MB` +
+                `\nStatus: Working [${client.user.tag}] => Logged in Successfully\n`
+        ).magenta
+    );
+})
+.catch((error) => {
+    console.log(
+        ColorOutput(
+            `\n=== Bot Login Info ===` +
+                `\n\nToken Length:                  ${process.env.TOKEN.length}` +
+                `\nNode Version:                   ${process.version}` +
+                `\nDevice Architecture:            ${process.arch}` +
+                `\nDiscord.js Version:             ${
+                    require("discord.js").version
+                }` +
+                `\nOS:                             ${process.platform}` +
+                `\nUptime:                         ${Math.floor(
+                    process.uptime()
+                )} seconds` +
+                `\nMemory Used:                    ${Math.round(
+                    (process.memoryUsage().heapUsed / 1024 / 1024) * 100
+                ) / 100} MB` +
+                `\nStatus: Failed [ERROR] => Login failed\n`
+        ).red
+    ); // Changed to red for errors
+    console.error(`Error Details: ${error.message}`); // Log the error details for debugging
+});
