609bc9f9599771b0e1d26c7648271a02f86a8300
Tristan | S. <simontristandev@gmail.com>
2024-10-12 14:01:21 +0200

    make a messagereply event function

---
+++ src/Event-Functions/Message-Send/Reply.js
@@ -0,0 +1,13 @@
+//require Components
+const Discord = require('discord.js')
+
+
+//define the function
+module.exports = {
+    async execute(message) {
+
+        if (!(message instanceof Discord.Message)) return;
+
+        message.reply("sdsxdddddd")
+    }
+}
