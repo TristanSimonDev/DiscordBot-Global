3c38f32c8df6c1f9a2a961e8f23781d1c820d2ff
Tristan | S. <simontristandev@gmail.com>
2024-10-13 18:11:26 +0200

    use dynamic sending

--- src/Event-Functions/Guild-Add/Send-Instructions.js
+++ src/Event-Functions/Guild-Add/Send-Instructions.js
@@ -7,21 +7,29 @@ module.exports = {
         if (!(guild instanceof Discord.Guild)) return;
 
 
-        //Create Category
-
+        // Create a category
         const category = await guild.channels.create({
             name: "help",
             type: Discord.ChannelType.GuildCategory
-        })
+        });
 
+        // Create a text or announcement channel within the category
         const tutorial_channel = await guild.channels.create({
             name: "tutorial-nova",
-            parent: category.id,
-            type: Discord.ChannelType.GuildAnnouncement
-        })
+            type: Discord.TextChannel, // Announcement channel (or change to 'GuildText' for a text channel)
+            parent: category.id // Set the parent to the category you just created
+        });
+
+        const greeting = new Discord.EmbedBuilder()
+            .setTitle("Thanks for using Nova bot")
+
+        const tutorial_embeds = [greeting]
+
+        await tutorial_channel.send({ embeds: [...tutorial_embeds] })
 
 
 
 
+        
     }
 }
