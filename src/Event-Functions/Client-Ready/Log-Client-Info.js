c6075cce50d696cce4e9eb588baab5bfeb7b5901
Tristan | S. <simontristandev@gmail.com>
2024-10-13 13:36:03 +0200

    log the info via object.entries

--- src/Event-Functions/Client-Ready/Log-Client-Info.js
+++ src/Event-Functions/Client-Ready/Log-Client-Info.js
@@ -9,23 +9,21 @@ module.exports = {
         //check if the Client is an DiscordClient, when not then return and do nothing 
         if (!(client instanceof Discord.Client)) return;
 
-        console.log(client.user)
-
-        //defiene the Client-Info-Output in an 2 arry output 
-        const client_info = [
-            ["Username:", client.user.username],
-            ["Id:", client.user.id],
-            ["Tag:", client.user.tag],
-            [],
-            [],
-            ["Avatar-Hash:", client.user.avatar],
-            ["Verified:", client.user.verified],
-            ["Bot?", client.user.bot],
-        ]
+        
+        const client_info_fields = {
+            Username: client.user.username,
+            Id: client.user.id,
+            Tag: client.user.tag,
+            Avatar_Hash: client.user.avatar,
+            Verified: client.user.verified,
+            Bot: client.user.bot
+        }
 
         //Use foreach to log the single arrays
-        client_info.forEach(info_array => {
-            console.log(...info_array)
+
+        Object.entries(client_info_fields).forEach(([Key, Value]) => {
+            console.log(`${Key}: ${Value}`)
         })
+
     }
 }
