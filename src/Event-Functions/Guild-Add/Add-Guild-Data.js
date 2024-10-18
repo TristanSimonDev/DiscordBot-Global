1a9dbd62a0be591b2ea5201d23230e44d664559a
Tristan | S. <simontristandev@gmail.com>
2024-10-18 15:53:37 +0200

    add interfaces

--- src/Event-Functions/Guild-Add/Add-Guild-Data.js
+++ src/Event-Functions/Guild-Add/Add-Guild-Data.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
-const path = require('path')
 const Discord = require('discord.js')
+
+let channels_interface = {
+    "announcement_channel": "",
+    "ticket_channel": "",
+    "log_channel": ""
+}
 
 module.exports = {
     async execute(guild) {
@@ -15,13 +20,11 @@ async function CreateFolder_Files(folderPath, Guild_Id) {
 
     const data_folder = fs.mkdirSync(`${folderPath}/${Guild_Id}`, { recursive: true, mode: 0o777 });
     
-    fs.writeFileSync(`${data_folder}/channels.json`,
-`{
-    "announcement_channel": "",
-    "ticket_channel": ""
-}`);
+    fs.writeFileSync(`${data_folder}/channel_id.json`, JSON.stringify(channels_interface, null, 2), 'utf8');
     
-    fs.writeFileSync(`${data_folder}/users.json`, "{}");
+    fs.writeFileSync(`${data_folder}/users_data.json`, "{}");
+
+    fs.writeFileSync(`${data_folder}/ticket_data.json`, "{}")
 }
 
-let data = CreateFolder_Files("Data/Guilds", "tes34234t")
+//let data = CreateFolder_Files("Data/Guilds", "tes34234t")
