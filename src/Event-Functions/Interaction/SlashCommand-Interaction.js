d712eb5a8a35a47efa49d435a7001a271c96fcb1
Tristan | S. <simontristandev@gmail.com>
2024-10-18 16:29:15 +0200

    the reply file of the SlashCommandInteraction

---
+++ src/Event-Functions/Interaction/SlashCommand-Interaction.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    async execute(interaction) {
+        if (!(interaction instanceof Discord.ChatInputCommandInteraction)) return;
+
+    
+    
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+    	if (!command) {
+    		console.error(`No command matching ${interaction.commandName} was found.`);
+    		return;
+    	}
+    
+    	try {
+    		await command.execute(interaction);
+    	} catch (error) {
+    		console.error(error);
+    		if (interaction.replied || interaction.deferred) {
+    			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    		} else {
+    			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    		}
+    	}
+    
+    
+    
+    }
+}
