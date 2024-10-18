6d9ece7bdf86b98e77ed00fa862d5f28864a50c8
Tristan | S. <simontristandev@gmail.com>
2024-10-18 16:39:25 +0200

    make the interaction file in events

---
+++ src/Events/Interaction.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+
+
+module.exports = {
+    name: Discord.Events.GuildCreate,
+    type: 'on',
+    id: 3,
+
+    async execute(interaction) {
+        if (interaction instanceof Discord.ChatInputCommandInteraction) {
+            const Event = require('../Event-Functions/Interaction/SlashCommand-Interaction')
+            Event.execute(interaction)
+        }
+
+        
+
+
+
+        
+    }
+}
