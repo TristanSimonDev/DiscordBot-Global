2c35fff6b3607a8a50e15d69ecbd4b693e64ea66
Tristan | S. <simontristandev@gmail.com>
2024-10-26 20:28:41 +0200

    use the channel template

--- src/Event-Functions/Guild-Add/Add-Guild-Data.js
+++ src/Event-Functions/Guild-Add/Add-Guild-Data.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 const Discord = require('discord.js')
-
-let channels_interface = {
-    "announcement_channel": "",
-    "ticket_channel": "",
-    "log_channel": ""
-}
+let { channel_template } = require('.vscode/Templates/Channel_Template.js')
 
 module.exports = {
     async execute(guild) {
@@ -20,7 +15,7 @@ async function CreateFolder_Files(folderPath, Guild_Id) {
 
     const data_folder = fs.mkdirSync(`${folderPath}/${Guild_Id}`, { recursive: true, mode: 0o777 });
     
-    fs.writeFileSync(`${data_folder}/channel_id.json`, JSON.stringify(channels_interface, null, 2), 'utf8');
+    fs.writeFileSync(`${data_folder}/channel_id.json`, JSON.stringify(channel_template, null, 2), 'utf8');
     
     fs.writeFileSync(`${data_folder}/users_data.json`, "{}");
 
