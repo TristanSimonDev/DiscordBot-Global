bf3ceea290819400894ddc3de698e5793fcae44b
Tristan | S. <simontristandev@gmail.com>
2024-10-26 20:28:13 +0200

    complete the slashcommand delte_all_channel_data

--- src/Commands/Admin/Delete-All-Channels-in-Data.js
+++ src/Commands/Admin/Delete-All-Channels-in-Data.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
 const fs = require('fs');
+const { channel_template } = require('../../../.vscode/Templates/Channel_Template');
 const wait = require('node:timers/promises').setTimeout;
 
 
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
-        .setName("add_channel")
+        .setName("delete_channels")
         .setDescription("This command will the delete all your channels you saved")
         .addStringOption(option =>
             option
@@ -14,8 +15,8 @@ module.exports = {
                 .setDescription("Are you sure?")
                 .setRequired(true)
                 .addChoices(
-                    { name: "Yes", value: "announcement_channel" },
-                    { name: "No", value: "ticket_channel" },
+                    { name: "Yes", value: "Yes" },
+                    { name: "No", value: "No" },
                 )
         ),
         
@@ -23,59 +24,42 @@ module.exports = {
 
     async execute(interaction) {
         if (!(interaction instanceof Discord.CommandInteraction)) return;
-    
-        const channel_id = interaction.channel.id;
-        const guild_id = interaction.guild.id;
-        const dynamic_channel_selector = interaction.options.get("channels").value;
-    
-        const data = fs.readFileSync(`./Data/Guilds/${guild_id}/channel_id.json`, 'utf8');
-        const parsed_data = JSON.parse(data);
-    
+
+        const guild_id = interaction.guild.id
+        const option = interaction.options.get("delete_channels").value
+
+        let data = fs.readFileSync(`./Data/Guilds/${guild_id}/channel_id.json`, 'utf8');
+        let parsed_data = JSON.parse(data);
+
         const success_embed = new Discord.EmbedBuilder()
-            .setDescription(`Successfully verified \`[${dynamic_channel_selector} ${channel_id}]\` in the Database`);
-    
-        const channel_already_added_embed = new Discord.EmbedBuilder()
-            .setDescription(`The channel with ID \`${channel_id}\` is already added as ${dynamic_channel_selector}.`);
-    
+            .setDescription("Succesfully cleared channel_id Data")
+        
+        const not_deleted_embed = new Discord.EmbedBuilder()
+            .setDescription("They interrupted the process")
+
         try {
-            // Find if the entry already exists in the parsed data
-            const existingEntry = Object.entries(parsed_data).find(([key, value]) => key == dynamic_channel_selector);
-    
-            if (existingEntry) {
-                const [key, value] = existingEntry;
-    
-                // If channel ID matches, send already added embed
-                if (value !== "") {
-                    await interaction.deferReply()
-                    await wait(2_000)
-                    await interaction.editReply({ embeds: [channel_already_added_embed], ephemeral: true }); 
-                    
-                    return 
-                } else {
 
-                    // If the key matches but the channel ID doesn't, notify and update it
-                    parsed_data[dynamic_channel_selector] = channel_id;
-                    fs.writeFileSync(`./Data/Guilds/${guild_id}/channel_id.json`, JSON.stringify(parsed_data, null, 2), 'utf-8');
-                    return interaction.reply({ embeds: [success_embed], ephemeral: true });
-                    
-                }
+            if (option == "Yes") {
+                
+                //write the empty template
+                parsed_data = channel_template
+                fs.writeFileSync(`./Data/Guilds/${guild_id}/channel_id.json`, JSON.stringify(parsed_data, null, 2), 'utf-8')
+
+                //show Nova is thinking...
+                await interaction.deferReply()
+                //wait 2s
+                await wait(2_000)
+                //send the embed after 2 seconds
+                await interaction.editReply({ embeds: [success_embed], ephemeral: true });
+
             } else {
-
-                // If no match was found, add the new entry
-                parsed_data[dynamic_channel_selector] = channel_id;
-                fs.writeFileSync(`./Data/Guilds/${guild_id}/channel_id.json`, JSON.stringify(parsed_data, null, 2), 'utf-8');
-                return interaction.reply({ embeds: [success_embed], ephemeral: true });
-
-            }
+                //if the awnser is no just return
+                return
     
-        } catch (error) {
+            }
 
-            console.error(error);
-            const error_embed = new Discord.EmbedBuilder()
-                .setDescription(`Error while trying to save \`[${dynamic_channel_selector} ${channel_id}]\` in the Database`);
-            return interaction.reply({ embeds: [error_embed], ephemeral: true });
-
-        }
+        } catch (err) {console.error(err)}
+        
     }
         
     
