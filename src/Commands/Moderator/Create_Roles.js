f8389864c4abb28fefa2d9012905ae1c7af5b175
Tristan | S. <simontristandev@gmail.com>
2024-10-28 20:53:27 +0100

    add colors

--- src/Commands/Moderator/Create_Roles.js
+++ src/Commands/Moderator/Create_Roles.js
@@ -7,7 +7,7 @@ module.exports = {
         .addStringOption(option => 
             option   
                 .setName('role_name')
-                .setDescription("what role do you want to choose?")
+                .setDescription("what role name do you want to choose?")
                 .addChoices(
                     { name: "Admin", value: "Admin" },
                     { name: "Moderator", value: "Moderator" },
@@ -19,12 +19,40 @@ module.exports = {
                     { name: "Helper", value: "Helper" },
                     { name: "Guardian", value: "Guardian" },
                     { name: "Champion", value: "Champion" }
-                )
+            )
+    )
+        .addStringOption(option => 
+            option   
+                .setName('role_color')
+                .setDescription("what role color do you want to choose?")
+                .addChoices(
+                    { name: "Red", value: "#FF0000" },
+                    { name: "Orange", value: "#FFA500" },
+                    { name: "Yellow", value: "#FFFF00" },
+                    { name: "Green", value: "#008000" },
+                    { name: "Blue", value: "#0000FF" },
+                    { name: "Purple", value: "#800080" },
+                    { name: "Pink", value: "#FFC0CB" },
+                    { name: "Teal", value: "#008080" },
+                    { name: "Cyan", value: "#00FFFF" },
+                    { name: "Magenta", value: "#FF00FF" },
+                    { name: "Brown", value: "#A52A2A" },
+                    { name: "Black", value: "#000000" },
+                    { name: "White", value: "#FFFFFF" },
+                    { name: "Gray", value: "#808080" }
+            )
         ),
-    
+        
     async execute(interaction) {
         if (!(interaction instanceof Discord.CommandInteraction)) return;
 
+        const role_name = interaction.options.get("role_name").value
+        const role_color = interaction.options.get("role_color").value
+
+        interaction.guild.roles.create({
+            name: role_name,
+            color: role_color
+        })
 
 
     }
