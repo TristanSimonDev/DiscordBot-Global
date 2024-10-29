7e8a34954ff5c857cdde9fb545cf61269375f30a
Tristan | S. <simontristandev@gmail.com>
2024-10-29 13:11:36 +0100

    add server (express)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const Discord = require('discord.js')
-const dotenv = require('dotenv').config()
-const fs = require('fs')
+// Required modules
+const Discord = require('discord.js');
+const dotenv = require('dotenv').config();
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { ColorOutput } = require('../modules/ColorOutput');
 
-const { ColorOutput } = require('../modules/ColorOutput')
+
+//--------------------------------------------------------------------Client Events
 
 const client = new Discord.Client({ intents: [process.env.intents] })
 
@@ -15,6 +21,38 @@ for (const file of EventFiles) {
     ColorOutput(`Event: [${Event.name}] with EventId [${Event.id}] loaded successfully`).green()
     client[Event.type](Event.name, async (...args) => Event.execute(...args))
 }
+
+//----------------------------------------------------------------------------
+
+
+// Set up Express server
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the HTML form at the root URL
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname,'../Web/Admin.html'); // Use absolute path
+    res.sendFile(filePath);
+});
+
+// Route to receive the message from the HTML form
+app.post('/send-message', async (req, res) => {
+    const message = req.body.message;
+
+    try {
+        const channel = await client.channels.fetch("1298641564712829010"); // Replace with your channel ID
+        await channel.send(message);
+        res.send('Message sent to Discord!');
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).send('Failed to send message to Discord.');
+    }
+});
+
+// Start the Express server
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
 
 client.login(process.env.TOKEN)
 .then(() => {
