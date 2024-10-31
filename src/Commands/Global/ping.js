bc7295438fecf9ba7b56752bae9d583b16ff3828
Tristan | S. <simontristandev@gmail.com>
2024-10-31 11:15:10 +0100

    make the command avaiable for everone

--- src/Commands/Global/ping.js
+++ src/Commands/Global/ping.js
@@ -1,15 +1,17 @@
 const Discord = require("discord.js")
-const { ColorOutput } = require('../../../modules/ColorOutput')
+const { ColorOutput } = require('../../../modules/ColorOutput');
+const Permission_bits = require('../../../modules/Permission_Bits')
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Shows the Bots reaction time in ms'),
+        .setDescription('Shows the Bots reaction time in ms')
+    .setDefaultMemberPermissions(Permission_bits.EVERYONE),
     
     async execute(interaction) {
         if (!(interaction instanceof Discord.CommandInteraction)) return;
 
-        interaction.reply(`Ping: ${interaction.client.ws.ping}`)
+        interaction.reply(`Ping: ${interaction.client.ws.ping}ms`)
 
         ColorOutput(`The user ${interaction.user.tag} - ${interaction.user.id} in Guild ${interaction.guild.name} - ${interaction.guild.id} used the ping command`).cyan()
     }
