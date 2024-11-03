9f0a3b9cf66c882b83896dfb4ed4e2da230da8d1
Tristan | S. <simontristandev@gmail.com>
2024-11-03 17:01:58 +0100

    test 1

---
+++ Data/Sql/DataBase.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+const mysql = require('mysql')
+const dotenv = require('dotenv').config()
+
+let db = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    port: process.env.port,
+    database: process.env.database
+});
+
+db.connect((err) => {
+    if (err) return console.error(err);
+    console.log("Connected");
+    db.query("CREATE TABLE IF NOT EXISTS Guild_Data (name VARCHAR(50), id VARCHAR(50))", (err, result) => {
+        if (err) return console.error(err);
+        console.log("Table checked/created");
+    });
+});
+
+let Guild_Data = ((Guild) => {
+    if (!(Guild instanceof Discord.Guild)) return;
+    db.query(`Insert Into Guild_Data (name, id) VALUE (?, ?)`, [Guild.name, Guild.id], (err, res) => {
+        if (err) return console.error(err);
+        console.log("done");
+        
+    })
+})  
+
