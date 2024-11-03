428249bacebb9d0099414c4212c7ed9bf4e17267
Tristan | S. <simontristandev@gmail.com>
2024-11-03 17:12:14 +0100

    remove all other stuff to simplify it more

--- src/Event-Functions/Guild-Add/Add-Guild-Data.js
+++ src/Event-Functions/Guild-Add/Add-Guild-Data.js
@@ -1,23 +1,10 @@
-const fs = require('fs')
 const Discord = require('discord.js')
-let  channel_template = require('../../../.vscode/Templates/Channel_Template').channel_template
+const sql = require('../../../Data/Sql/DataBase')
 
 module.exports = {
     async execute(guild) {
         if (!(guild instanceof Discord.Guild)) return;
 
-        // Create a Folder in Data\Guilds with the Guild Id
-        CreateFolder_Files("Data/Guilds", `${guild.id}-${guild.name}`)
+        sql.Guild_Data(guild)
     }
-}
-
-async function CreateFolder_Files(folderPath, Guild_Id_Guild_Name) {
-
-    const data_folder = fs.mkdirSync(`${folderPath}/${Guild_Id_Guild_Name}`, { recursive: true, mode: 0o777 });
-    
-    fs.writeFileSync(`${data_folder}/channel_id.json`, JSON.stringify(channel_template, null, 2), 'utf8');
-    
-    fs.writeFileSync(`${data_folder}/users_data.json`, "{}");
-
-    fs.writeFileSync(`${data_folder}/ticket_data.json`, "{}")
 }
